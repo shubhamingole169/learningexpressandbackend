fe5509ba95dfcc87d1663f1aef9383c1b92f78fc
Shubham Ingole <shubhamingole169@gmail.com>
2025-03-10 13:26:45 +0530

    resolve errror

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,37 +1,3 @@
-// import multer from "multer";
-
-// // Configure Storage
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, './public/temp'); // Ensure this folder exists
-//     },
-//     filename: function (req, file, cb) {
-//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-//         cb(null, file.fieldname + '-' + uniqueSuffix + '-' + file.originalname);
-//     }
-// });
-
-// // Initialize Multer
-// export const upload = multer({ storage })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import multer from "multer";
 
 const storage = multer.diskStorage({
