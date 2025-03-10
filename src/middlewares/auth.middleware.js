f3bb05694959f52f99eb2f6da2c57e8bfc50b0c2
Shubham Ingole <shubhamingole169@gmail.com>
2025-03-10 18:05:07 +0530

    login and logout error resolve

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,12 +1,15 @@
-import { User } from "../models/user.model";
-import { ApiError } from "../utils/ApiError";
-import { asyncHandler } from "../utils/asyncHandler";
-import { jwt } from 'jsonwebtoken';
+import { User } from "../models/user.model.js";
+
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from 'jsonwebtoken';
+
 
 
 export const verifyJWt = asyncHandler( async( req, res, next) => {
     try {
         const token = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer", "")
+
     
         if(!token) {
             throw new ApiError(401, "Unauthorized request")
