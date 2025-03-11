8972765dadf7ede1b4fcfaeb52b03c2d1cf93275
Shubham Ingole <shubhamingole169@gmail.com>
2025-03-11 15:26:24 +0530

    some error resolve

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -188,8 +188,13 @@ const ChangeCurrentPassword = asyncHandler(async(req, res) => {
 })
 
 const getCurrentUser = asyncHandler(async(req, res) => {
-    return res.status(200)
-    .json(200, req.user, "current user fetched  sucessfully !!!")
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        req.user,
+        "current user fetched  sucessfully !!!"
+    ))
 })
 
 const updateAccountDetails = asyncHandler(async(req, res) => {
